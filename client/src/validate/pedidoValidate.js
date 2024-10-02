d2d5b9a20d0fb3efe99e52b960d8424b061b953c
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-10-02 16:29:48 -0500

    Test 1 render

--- client/src/validate/pedidoValidate.js
+++ client/src/validate/pedidoValidate.js
@@ -1,4 +1,4 @@
-import z from "zod"
+import { z } from "zod"
 
 const pedidoValidateSchemma = z.object({
   usuario_id: z.number({
--- client/src/validate/tokenValidate.js
+++ client/src/validate/tokenValidate.js
@@ -1,4 +1,4 @@
-import z from "zod"
+import { z } from "zod"
 
 const tokenPassSchemma = z.object({
   token_pass: z.number({
